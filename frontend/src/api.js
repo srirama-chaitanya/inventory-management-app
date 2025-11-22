0ceb73ee99853d5b41eb1f435ee1625ef15a8460
Srirama Chaitanya <sriramchaitanya06@gmail.com>
2025-11-23 00:11:19 +0530

    Update API to use Environment Variable

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Create a custom Axios instance
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    // Look for the LIVE URL first, otherwise use Localhost
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
 
 // --- INTERCEPTOR ---
